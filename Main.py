db22c5d4e3ad7fed01f5545ce67456fd77fba9ee
vosxodd <73089079+vosxodd@users.noreply.github.com>
2021-04-05 09:34:14 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -10,15 +10,5 @@ for n in range(n):
     triangle_list.append(line_string)
 print(triangle_list)
 
-c = len(triangle_list[-1])
-counter = 1
-i = 0
 for raw in triangle_list:
-    if counter % 2 != 0:
-        line_string = []
-        for number in line:
-        print(' ' * (c - i), triangle_list[i])
-    elif counter % 2 == 0:
-        print(' ' * (c - i - 1), triangle_list[i])
-    i += 1
-    counter += 1
+    print(raw.center(100))
